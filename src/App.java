07911b73e94efded38db2a3ac0a4e6ad390647f2
Pedro Perez <pipr96@gmail.com>
2023-06-25 22:23:25 -0700

    getters and setters

--- src/App.java
+++ src/App.java
@@ -2,11 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Dog d = new Dog();
-        d.name = "Loki";
-        d.age = 1;
+        d.setName("Loki");
+        d.setAge(1);
         d.bark();
         d.jump(3);
-        
+
     }
 }
 
--- src/Dog.java
+++ src/Dog.java
@@ -1,12 +1,23 @@
+import java.util.jar.Attributes.Name;
 public class Dog {
-    String name = "";
-    Integer age = 0;
+    private String name;
+    private Integer age;
+    public void setName(String givenName){
+        name = givenName;
+    }
+    public void setAge( Integer givenAge){
+        age = givenAge;
+    }
+    public String getName(){
+        return name;
+    }
     void bark(){
+        
         System.out.println(name + " barks");
     }
     void jump(int times){
         while (times > 0){
-            System.out.println(name + "jumps!");
+            System.out.println(name + " jumps!");
             times = times - 1;
         }
     }
